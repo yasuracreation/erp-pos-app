c8397305ae8f54e85bc07f391f642e595149fbee
Yasura <yasuracreation@gmail.com>
2021-08-08 23:21:13 +0530

    erp setup module added

---
+++ projects/erp-setup/src/app/cor/ver1/users/request/user-request.model.ts
@@ -0,0 +1,9 @@
+export interface UserRequest{
+    userId:number,
+    userName:string,
+    firstName:string,
+    lastName:string,
+    contactNumber:string,
+    userGroupId:number,
+    userPrevilages:string
+}
